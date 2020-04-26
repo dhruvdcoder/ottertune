afd213c1f29e88b803fa648681ec79f2e334195c
Dhruvesh <dhruvdplayer@gmail.com>
2020-04-25 22:33:45 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    
+]
+
+setup(
+    name='ottertune',
+    version='0.0.0',
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=install_requires,
+    packages=find_packages("server"),
+    #package_dir={"": "server"},   # tell distutils packages are under src
+    include_package_data=True,    # include everything in source control
+    python_requires='>=3.6')
