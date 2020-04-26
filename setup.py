3d0e1a6b950e3b27c693d788852586dd1cd4a8d4
Dhruvesh <dhruvdplayer@gmail.com>
2020-04-25 22:35:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ install_requires = [
 setup(
     name='ottertune',
     version='0.0.0',
-    packages=find_packages(
+    packages=find_packages("server",
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=install_requires,
-    packages=find_packages("server"),
+    
     #package_dir={"": "server"},   # tell distutils packages are under src
     include_package_data=True,    # include everything in source control
     python_requires='>=3.6')
